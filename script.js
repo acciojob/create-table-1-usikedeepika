b94f76e300ead60a54736156eee6514ebae4dc3c
Usike deepika <111295716+usikedeepika@users.noreply.github.com>
2025-04-21 20:30:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
 function insert_Row() {
-    //Write your code here
+    
+  let tab=document.getElementById("mytable")
+
+var j=1;
+var i=2;
+
+let head=document.getElementById("myhead")
+
+function insert_Row(){
+ 
+   let elem=document.createElement("tr");
+    let data1=document.createElement("td");
+
+    let data2=document.createElement("td");
+     
+    i+=1;
+
+    data1.textContent+=`New cell${j++}`
+    data2.textContent+=`New cell${j++}`;
+
+    elem.appendChild(data1);
+    elem.appendChild(data2)
+
+    head.appendChild(elem);
   
+    // tab.appendChild(elem)
+
+    j=1;
+
+ 
+
+    
+
+}
   
 }

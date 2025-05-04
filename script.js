12d88e9bb990af03d19506f4c13f7205bd1967f6
Usike deepika <111295716+usikedeepika@users.noreply.github.com>
2025-05-04 15:13:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,38 @@
-function insert_Row() {
-    
-  let tab=document.getElementById("mytable")
-
-var j=1;
-var i=2;
-
-let head=document.getElementById("myhead")
-
-function insert_Row(){
- 
-   let elem=document.createElement("tr");
-    let data1=document.createElement("td");
-
-    let data2=document.createElement("td");
-     
-    i+=1;
-
-    data1.textContent+=`New cell${j++}`
-    data2.textContent+=`New cell${j++}`;
-
-    elem.appendChild(data1);
-    elem.appendChild(data2)
-
-    head.appendChild(elem);
-  
-    // tab.appendChild(elem)
-
-    j=1;
-
- 
 
     
-
-}
+    let tab=document.getElementById("mytable")
   
-}
+  var j=1;
+  var i=2;
+  
+  let head=document.getElementById("myhead")
+  
+  function insert_Row(){
+   
+     let elem=document.createElement("tr");
+      let data1=document.createElement("td");
+  
+      let data2=document.createElement("td");
+       
+      i+=1;
+  
+      data1.textContent+=`New cell${j++}`
+      data2.textContent+=`New cell${j++}`;
+  
+      elem.appendChild(data1);
+      elem.appendChild(data2)
+  
+      head.appendChild(elem);
+    
+      // tab.appendChild(elem)
+  
+      j=1;
+  
+   
+  
+      
+  
+  }
+    
+
+  
